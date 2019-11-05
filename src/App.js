19d74b7e52bae82f1c3090dbb615a8297133e767
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-04 22:51:19 -0500

    [Fix] Code clean up

--- src/App.js
+++ src/App.js
@@ -12,31 +12,7 @@ function App() {
   return (
     <div className="container">
       <ScoreBoard  homeScore={homeScore} awayScore={awayScore}/>
-      {/* <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-            <div className="home__score">{homeScore}</div>iv clas
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section> */}
       <Buttons homeScore={homeScore} setHomeScore={setHomeScore} awayScore={awayScore} setAwayScore={setAwayScore} />
-      {/* <section className="buttons">
-        <div className="homeButtons">
-          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={()=> setAwayScore(awayScore + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={()=> setAwayScore(awayScore + 3)}>Away Field Goal</button>
-        </div>
-      </section> */}
     </div>
   );
 }
